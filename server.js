5fce34deb3574151a0675f674f6ee9052b1f6fff
pathymavuba <mavubapathy@gmail.com>
2023-06-06 08:23:24 +0100

    master:create server

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const http = require('http');
 const PORT = process.env.PORT || 3000
 
 
-const server = http.createServer(async function(){
+const server = http.createServer(async function(req,res){
     //set request route 
-    
+    if (req.url === "/" && req.method === "GET") {
+      //response headers
+      res.writeHead(200, { "Content-Type": "application/json" });
+      //set the response
+      res.write("Hi there, This is a Vanilla Node.js API");
+      //end the response
+      res.end();
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not found" }));
+    }
 })
+
+server.listen(PORT,()=>console.log(`server is listenning on ${PORT} `))
